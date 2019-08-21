b65e10029419ba3d144b97e9bb4d4903bf57a55b
L.R.L <DaleLoreNY@gmail.com>
2019-08-21 13:23:06 -0400

    Added more content to homepage

--- src/Pages/English/StepOne.js
+++ src/Pages/English/StepOne.js
@@ -5,16 +5,16 @@ const StepOne = props => {
 
   return (
     <div className="steps">
-      <h1 id="instruction-header">Set out your archive's mission and objectives</h1>
+      <h1 id="instruction-header">Step 1: Set out your archive's mission and objectives</h1>
       <ol>
-      <li>Whose stories do you want to preserve and/or amplify?</li>
-      <li>Decide on what kind of formats you want/can save</li>
-      <li>Define the period of time, scope and geographical area</li>
-      <li>Who do you want to tell that story/stories to? Why should it be YOU
+      <li id="instruction-li">Whose stories do you want to preserve and/or amplify?</li>
+      <li id="instruction-li">Decide on what kind of formats you want/can save</li>
+      <li id="instruction-li">Define the period of time, scope and geographical area</li>
+      <li id="instruction-li">Who do you want to tell that story/stories to? Why should it be YOU
       making this project and what’s your relationship to this content</li>
-      <li>Establish the resources that each objective will need – people, partners,
+      <li id="instruction-li">Establish the resources that each objective will need – people, partners,
       premises, equipment?</li>
-      <li>Define your budgetary needs – how much money will this project need to
+      <li id="instruction-li">Define your budgetary needs – how much money will this project need to
       succeed?</li>
       </ol>
     </div>
--- src/Pages/English/avItems.js
+++ src/Pages/English/avItems.js
@@ -6,8 +6,15 @@ class AvItems extends Component {
   render() {
     return (
       <div>
-      <h1 id="instruction-header">What you got</h1>
+      <h1 id="instruction-header">What you got?</h1>
+        <ul>
+          <li id="instruction-li"><strong>PAPER:</strong> If you can, use acid-free folders and boxes. Remove metal clips and staples. Do not fold, glue or laminate. Wash your hands before handling any originals. Store papers away from light, moisture and heat.</li>
+          <li id="instruction-li"><strong>VIDEO:</strong> Avoid touching the tape Store fully rewound and vertically Keep away from magnetic forces (electrical appliances) Label your tapes with dates and identifying information. Use archival-quality adhesive labels. Ensure future access by digitizing your collection</li>
+          <li id="instruction-li"><strong>FILM:</strong> Handle by the center of the hub (reel to reel), only touch the sides and keep fingers away from the image. Wear cotton gloves if you need to clean and repair Store in low-humidity, low-light, and consistent temperature (preferably low temp) Don't store plastic bags, you may use cardboard boxes or original metal/plastic containers.</li>
+          <li id="instruction-li"><strong>PHOTOGRAPHS:</strong> Handle photographs and negatives with gloves. Fingerprints can cause permanent stains on photographs and negatives. Protect from light Place photos in acid free boxes or sleeves. Avoid laminated photo albums. Glue attracts pests and insects. Store in low temperatures</li>
+          <li id="instruction-li"><strong>DIGITAL MEDIA:</strong> Migrate data on formats that are obsolete or near-obsolete (i.e. floppys or CDs) Access your files and/or storage every 6 months. Apply the 3-2-1 rule: Make three copies, use two different types of media for storing your copies, store one copy offsite. Make access copies. Note that life expectancy of digital storage varies: <br/> ~ DVD/CDs ~10 yrs <br/> ~ Hard drives, memory cards ~5-10 yrs <br/> ~Cloud storage depends on the company's terms of service (!)</li>
 
+        </ul>
       </div>
     );
   }
